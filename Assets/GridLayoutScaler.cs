be1d43c6981221ccfe48399b1fdbfa9cbf1999d5
IvanKorchmit <criperadmiral92@gmail.com>
2021-07-01 04:27:11 +0300

    Jinn

--- Assets/GridLayoutScaler.cs
+++ Assets/GridLayoutScaler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.U2D;
 public class GridLayoutScaler : MonoBehaviour
 {
     public Canvas canvas;
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "New Teleport", menuName = "Perks/Teleport")]
+public class Teleport : PerkClass
+{
+    public int damage;
+    public override void Use(GameObject owner)
+    {
+        if(owner.CompareTag("Enemy"))
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            owner.transform.position = player.transform.position;
+            player.GetComponent<IDamagable>().Damage(Random.Range(1, damage), owner);
+        }
+    }
+}
